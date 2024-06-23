20725d132a76be1206d1b1b16d50dbdc12556b12
sahil <sahil.saxena@student.manchester.ac.uk>
2024-06-23 09:04:21 +0100

    Eraser and Rainbow added

--- script.js
+++ script.js
@@ -9,31 +9,26 @@ function place_blocks(size){
         sketch_block.appendChild(block);
         block.addEventListener('mouseenter', () => {
             if (choice == 'eraser'){
-                block.style.backgroundColor = 'white';
-                
-
+                block.style.backgroundColor = 'white';     
+                isClear = true;          
             }else if (choice == 'rainbow'){
+                const color_list = ['#ff0000','#ff7f00','#ffff00','#7fff00','#00ff00','#00ff7f','#00ffff','#007fff','#0000ff','#7f00ff'
+                ]
+                let chosenColor = color_list[Math.floor(Math.random() * color_list.length)];
+                block.style.backgroundColor = chosenColor;
+                isClear = false;
 
             }else{
                 block.style.backgroundColor = 'black';
-            }            
+                isClear = false;  
+                
+            }   
+                    
             
         });
     }
 }
 
-function isClearOrNot(){
-    const blocks = document.querySelectorAll('.block-item');
-    isClear = true;
-    blocks.forEach((item) => {
-        // Use getComputedStyle to get the actual computed background color
-        const backgroundColor = window.getComputedStyle(item).backgroundColor;
-        if (backgroundColor !== 'rgb(255, 255, 255)' && backgroundColor !== 'white') {
-            isClear = false;
-        }
-    });
-    
-}
 
 function clear(){
     const blocks = document.querySelectorAll('.block-item');
@@ -54,6 +49,7 @@ const sketch_block = document.querySelector('.sketch-block');
 const clear_button = document.querySelector('#clear_button');
 let choice = 'color';
 const eraser_button = document.querySelector('#eraser-mode');
+const rainbow_button = document.querySelector('#rainbow-mode');
 const color_button = document.querySelector('#color-mode');
 const slider = document.querySelector('.slider');
 const slider_info = document.querySelector('#sliderValue');
@@ -65,25 +61,25 @@ place_blocks(size);
 clear_button.addEventListener('click',clear);
 eraser_button.addEventListener('click', () => 
     {
-        choice = 'eraser';
-        const blocks = document.querySelector('.block-item');
-        
+        choice = 'eraser';        
     }
 )
 
 color_button.addEventListener('click', () => 
     {
         choice = 'color';
-        const blocks = document.querySelector('.block-item');
-        
+    }
+)
+
+rainbow_button.addEventListener('click', () => 
+    {
+        choice = 'rainbow';
     }
 )
 
 
-
 slider.addEventListener('input',()=>
     {
-        isClearOrNot()
         if (isClear){
             size = slider.value;
             removeAllChildNodes(sketch_block);
