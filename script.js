ecec1eb2d12c8517ccb36b3d7b42bb324c792ab6
sahil <sahil.saxena@student.manchester.ac.uk>
2024-06-23 10:44:13 +0100

    Extra credit changes made

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 function place_blocks(size){
-
-    // In order to set a changeable grid size, We need to first take input from the user in the form of a length bar. 
     let block_length = (100/size);
     for (let i = 1; i <= (size * size); i++){
         const block = document.createElement('div');
         block.classList.add('block-item');
-        block.setAttribute('style',`border:1px solid;height:${block_length}%;width:${block_length}%;box-sizing:border-box;`);
+        block.setAttribute('style',`border:1px solid;height:${block_length}%;width:${block_length}%;box-sizing:border-box;opacity:1`);
         sketch_block.appendChild(block);
         block.addEventListener('mouseenter', () => {
             if (choice == 'eraser'){
-                block.style.backgroundColor = 'white';     
-                isClear = true;          
+                block.style.backgroundColor = 'white';    
+                block.style.opacity = '1'; 
+                isClear = true;         
             }else if (choice == 'rainbow'){
                 const color_list = ['#ff0000','#ff7f00','#ffff00','#7fff00','#00ff00','#00ff7f','#00ffff','#007fff','#0000ff','#7f00ff'
                 ]
                 let chosenColor = color_list[Math.floor(Math.random() * color_list.length)];
                 block.style.backgroundColor = chosenColor;
+                block.style.opacity = '1';
                 isClear = false;
+
+            }else if(choice == 'shading'){
+                const current_opacity_str = block.style.opacity;
+                const current_opacity = parseFloat(current_opacity_str) || 1;
+                let new_opacity = current_opacity - 0.1;
+
+                if (new_opacity < 0) {
+                    new_opacity = 1; 
+                }
+
+                block.style.opacity = new_opacity.toString();
+
+                if (block.style.backgroundColor === 'white' || block.style.backgroundColor === '') {
+                    block.style.backgroundColor = 'black';
+                }
+
 
             }else{
                 block.style.backgroundColor = 'black';
+                block.style.opacity = '1';
                 isClear = false;  
                 
             }   
@@ -34,6 +51,7 @@ function clear(){
     const blocks = document.querySelectorAll('.block-item');
     blocks.forEach((item) => {
         item.style.backgroundColor = 'White';
+        item.style.opacity = '1';
     })
     isClear = true;
 }
@@ -51,6 +69,7 @@ let choice = 'color';
 const eraser_button = document.querySelector('#eraser-mode');
 const rainbow_button = document.querySelector('#rainbow-mode');
 const color_button = document.querySelector('#color-mode');
+const shading_button = document.querySelector('#shading-mode');
 const slider = document.querySelector('.slider');
 const slider_info = document.querySelector('#sliderValue');
 let isClear  = true;
@@ -74,6 +93,12 @@ color_button.addEventListener('click', () =>
 rainbow_button.addEventListener('click', () => 
     {
         choice = 'rainbow';
+    }
+)
+
+shading_button.addEventListener('click', () => 
+    {
+        choice = 'shading';
     }
 )
 
